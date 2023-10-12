527090cf28759f53ba5f5e7fe3529643c4031ad0
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2023-10-12 15:46:01 +0530

    improved ui

--- src/Components/BottleSelector/BottleSelector.jsx
+++ src/Components/BottleSelector/BottleSelector.jsx
@@ -1,33 +1,58 @@
-import React from 'react'
-import './BottleSelector.css'
+import React, { useState } from 'react';
+import './BottleSelector.css';
 
-function BottleSelector({handleId}) {
-    
+function BottleSelector({ handleId }) {
+  const [selectedBottle, setSelectedBottle] = useState(null);
+
+  const handleImageClick = (bottleId) => {
+    setSelectedBottle(bottleId);
+    handleId(bottleId);
+  };
+
   return (
     <div className='bottles'>
-  <div className='bottle-item'>
-    <img src={require('../Assets/Images/0.3.png')} onClick={() => handleId(0)} alt='' />
-    <p>250ml</p>
-  </div>
-  <div className='bottle-item'>
-    <img src={require('../Assets/Images/0.33.png')} onClick={() => handleId(1)} alt='' />
-    <p>300ml</p>
-  </div>
-  <div className='bottle-item'>
-    <img src={require('../Assets/Images/1.5.png')} onClick={() => handleId(2)} alt='' />
-    <p>750ml</p>
-  </div>
-  <div className='bottle-item'>
-    <img src={require('../Assets/Images/0.5.png')} onClick={() => handleId(3)} alt='' />
-    <p>1L</p>
-  </div>
-  <div className='bottle-item'>
-    <img src={require('../Assets/Images/1.png')} onClick={() => handleId(4)} alt='' />
-    <p>2L</p>
-  </div>
-</div>
-
-  )
+      <div className={`bottle-item ${selectedBottle === 0 ? 'white' : ''}`}>
+        <img
+          src={require('../Assets/Images/0.3.png')}
+          onClick={() => handleImageClick(0)}
+          alt=''
+        />
+        <p>250ml</p>
+      </div>
+      <div className={`bottle-item ${selectedBottle === 1 ? 'white' : ''}`}>
+        <img
+          src={require('../Assets/Images/0.33.png')}
+          onClick={() => handleImageClick(1)}
+          alt=''
+        />
+        <p>300ml</p>
+      </div>
+      <div className={`bottle-item ${selectedBottle === 2 ? 'white' : ''}`}>
+        <img
+          src={require('../Assets/Images/1.5.png')}
+          onClick={() => handleImageClick(2)}
+          alt=''
+        />
+        <p>750ml</p>
+      </div>
+      <div className={`bottle-item ${selectedBottle === 3 ? 'white' : ''}`}>
+        <img
+          src={require('../Assets/Images/0.5.png')}
+          onClick={() => handleImageClick(3)}
+          alt=''
+        />
+        <p>1L</p>
+      </div>
+      <div className={`bottle-item ${selectedBottle === 4 ? 'white' : ''}`}>
+        <img
+          src={require('../Assets/Images/1.png')}
+          onClick={() => handleImageClick(4)}
+          alt=''
+        />
+        <p>2L</p>
+      </div>
+    </div>
+  );
 }
 
-export default BottleSelector
+export default BottleSelector;
